fbb1efa9f149bffba35471037f4265584b9add48
toonarmycaptain <toonarmycaptain@hotmail.com>
2021-05-21 01:34:02 -0500

    Flesh out BokehPlotter, add export methods.
    Add to requirements.txt selenium, and geckodriver-autoinstaller as a firefox
    driver to save images.
    
    Flesh out save_html_to_file/plot_PIL_Image_png/save_as_png/save_as_svg saving
    methods, using webdriver to produce html/images/PIL.Image.
    
    Add __initialise_webdriver staticmethod to supply webdriver when needed. Very
    slow function, in need of caching or speedup. Currently doesn't import or
    initialise until needed.
    
    Add x/y_axis_location arguments to allow centering axis  0,0) or as desired.
    
    Change .plot to basically wrap .show method, to show plot in browser, but with
    additional line if argument is passed.

--- parseplot/plot/bokeh/bokeh_plotter.py
+++ parseplot/plot/bokeh/bokeh_plotter.py
@@ -1,11 +1,22 @@
 """Bokeh plotter"""
 from pathlib import Path
-from typing import Union, Sequence, Optional
+from typing import (Optional,
+                    Sequence,
+                    TYPE_CHECKING,
+                    Union,
+                    )
 
 import PIL
-from bokeh import io
-from bokeh.io import show
+from bokeh.io.export import get_screenshot_as_png
+from bokeh.io import (export_png,
+                      export_svg,
+                      save,
+                      show,
+                      )
 from bokeh.plotting import figure
+
+if TYPE_CHECKING:
+    from selenium import webdriver
 
 
 class BokehPlotter:
@@ -18,6 +29,8 @@ class BokehPlotter:
                  title: str = None,
                  x_axis_label: str = None,
                  y_axis_label: str = None,
+                 x_axis_location: str = None,
+                 y_axis_location: str = None,
                  ) -> None:
         """
         Object wrapping bokeh plotting functionality.
@@ -36,14 +49,28 @@ class BokehPlotter:
 
         points must be a sequence, or sequence of sequences of of x,y tuples of int or float.
 
-        :param points: Sequence[Sequence[tuple[int, float]]]
+        :param points: Union[Sequence[tuple[int, float]],
+                               Sequence[Sequence[tuple[int, float]]]]
+
+        :param title: str title for chart
+        :param x_axis_label: str label for x axis
+        :param y_axis_label: str label for y axis
+        :param x_axis_location: int location of x-axis (in terms of y-axis)
+        :param y_axis_location: int location of y-axis (in terms of x-axis)
+        :return: None
         """
         self.title: Optional[str] = title
         self.x_axis_label: Optional[str] = x_axis_label
         self.y_axis_label: Optional[str] = y_axis_label
+        self.x_axis_location: Optional[str] = x_axis_location
+        self.y_axis_location: Optional[str] = y_axis_location
+
         self._plot: figure = figure(title=self.title,
                                     x_axis_label=x_axis_label,
                                     y_axis_label=self.y_axis_label)
+        self._plot.xaxis.fixed_location = self.x_axis_location
+        self._plot.yaxis.fixed_location = self.y_axis_location
+
         self.points: list[Sequence[tuple[int, float]]] = []
         if points:
             self.__add_lines(points)
@@ -55,13 +82,15 @@ class BokehPlotter:
                  legend_label: str = None,
                  line_color: str = None,
                  line_width: int = None,
-                 ):
+                 ) -> None:
         """
+        Add a line to the class' plot.
 
-        :param points:
-        :param legend_label:
-        :param line_color:
-        :param line_width:
+        :param points: tio
+        :param legend_label: Sequence[tuple[int, float]]
+        :param line_color: str
+        :param line_width: str
+        :return: None
         """
         self.points.append(points)
         line_args = {'x': [x[0] for x in points],
@@ -76,30 +105,93 @@ class BokehPlotter:
         self._plot.line(**line_args)
 
     def plot(self,
-             passed_points: list[tuple[int, float]] = None,
-             ) -> PIL.Image:
+             passed_points: Sequence[tuple[int, float]] = None,
+             ) -> None:
         """
-        Plots the class' points, adding a line if points are passed.
+        Show plot in browser, adding a line if points are passed.
 
-
-        :param passed_points:
-        :return:
+        :param passed_points: Sequence[tuple[int, float]]
+        :return: None
         """
         if passed_points:
             self.add_line(passed_points)
 
-        # need browser installed?
-        # self.plotted = io.export.get_screenshot_as_png(self._plot)
-        # return self.plotted
-        return show(self._plot)
+        return self.show_in_browser()
 
-    def save_to_file(self, filepath: Union[Path, str] = None) -> str:
+    def plot_PIL_Image_png(self) -> PIL.Image:
         """
-        Save the plot to the given filepath.
+        Returns plot as a PIL Image object
+
+        :return: PIL.Image object
+        """
+        return get_screenshot_as_png(self._plot, driver=self.__initialise_webdriver())
+
+    def save_html_to_file(self, filepath: Union[str, Path]) -> Union[str, Path]:
+        """
+        Save the html plot to the given filepath.
+
+        Appends .html extension if not given.
+
+        :param filepath: str
+        :return: str
+        """
+        extension = '.html'
+
+        if isinstance(filepath, Path):
+            filepath = filepath.with_suffix(extension)
+        elif not filepath.endswith(extension):  # Path does not have .endswith
+            filepath += extension
+        return save(self._plot, filename=filepath)
+
+    def save_as_png(self, filepath: Union[str, Path]) -> Union[str, Path]:
+        """
+        Save the plot to the given filepath as a png image.
+
+        Appends .png extension if not given.
+
+        export_png returns a list of strings, this taking first element returns
+        just the filepath
+
         :param filepath:
         :return:
         """
-        return io.save(self._plot, filename=filepath)
+        extension = '.png'
+
+        if isinstance(filepath, Path):
+            filepath = filepath.with_suffix(extension)
+        elif not filepath.endswith(extension):  # Path does not have .endswith
+            filepath += extension
+        export_png(self._plot, filename=filepath, webdriver=self.__initialise_webdriver())
+
+        return filepath
+
+    def save_as_svg(self, filepath: Union[str, Path]) -> Union[str, Path]:
+        """
+        Save the plot to the given filepath as a svg image.
+
+        Appends .svg extension if not given.
+
+        export_svg returns a list of strings, this taking first element returns
+        just the filepath
+
+        :param filepath:
+        :return:
+        """
+        extension = '.svg'
+
+        if isinstance(filepath, Path):
+            filepath = filepath.with_suffix(extension)
+        elif not filepath.endswith(extension):  # Path does not have .endswith
+            filepath += extension
+        export_svg(self._plot, filename=filepath, webdriver=self.__initialise_webdriver())
+
+        return filepath
+
+    def show_in_browser(self) -> None:
+        """
+        Shows plot in default browser.
+        """
+        show(self.plot)
 
     def __add_lines(self, points: Union[Sequence[tuple[int, float]],
                                         Sequence[Sequence[tuple[int, float]]]]) -> None:
@@ -117,3 +209,22 @@ class BokehPlotter:
                 self.add_line(line)
         except TypeError:  # Single list:
             self.add_line(points)
+
+    @staticmethod
+    def __initialise_webdriver() -> webdriver.Firefox:
+        """
+        Returns initialised Firefox webdriver.
+
+        Very slow.
+
+        :return: webdriver.Firefox
+        """
+        from selenium import webdriver
+        from selenium.webdriver.firefox.options import Options
+        import geckodriver_autoinstaller
+
+        geckodriver_autoinstaller.install()  # In case driver not installed.
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        return driver
