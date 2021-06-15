c53a8d60b815f958efc161f8332c0b7bbb43a8eb
toonarmycaptain <toonarmycaptain@hotmail.com>
2021-06-15 00:29:38 -0500

    Add test for basic attrs, add_line, plot methods

--- tests/test_plot/test_bokeh/test_bokeh_plotter.py
+++ tests/test_plot/test_bokeh/test_bokeh_plotter.py
@@ -13,3 +13,86 @@ from src.parseplot.plot.bokeh.bokeh_plotter import BokehPlotter
 def test__init__(test_points, points_attr):
     test_plotter = BokehPlotter(test_points)
     assert test_plotter.points == points_attr
+
+
+class TestBokehPlotterAttrs:
+    def test_title(self):
+        test_title = 'some title'
+        test_plotter = BokehPlotter(title=test_title)
+        assert test_plotter.title == test_title
+
+    def test_x_axis_label(self):
+        test_x_axis_label = 'some x_axis_label'
+        test_plotter = BokehPlotter(x_axis_label=test_x_axis_label)
+        assert test_plotter.x_axis_label == test_x_axis_label
+
+    def test_y_axis_label(self):
+        test_y_axis_label = 'some y_axis_label'
+        test_plotter = BokehPlotter(y_axis_label=test_y_axis_label)
+        assert test_plotter.y_axis_label == test_y_axis_label
+
+    def test_x_axis_location(self):
+        test_x_axis_location = 'some x_axis_location'
+        test_plotter = BokehPlotter(x_axis_location=test_x_axis_location)
+        assert test_plotter.x_axis_location == test_x_axis_location
+
+    def test_y_axis_location(self):
+        test_y_axis_location = 'some y_axis_location'
+        test_plotter = BokehPlotter(y_axis_location=test_y_axis_location)
+        assert test_plotter.y_axis_location == test_y_axis_location
+
+
+class TestAddLine:
+    def test_add_line_args_forwarded(self):
+        """Simple test that points/args are forwarded to self._plot.line()"""
+        test_points = [(1, 2), (3, 4), (5, 6)]
+        test_x_points = [1, 3, 5]
+        test_y_points = [2, 4, 6]
+        test_legend_label = 'test_legend_label'
+        test_line_color = 'test_line_color'
+        test_line_width = 314
+
+        test_plotter = BokehPlotter()
+
+        # patch _plot.line
+        class TestPlotLine:
+            def line(self, **args):
+                print(args)
+                assert args['x'] == test_x_points
+                assert args['y'] == test_y_points
+                assert args['legend_label'] == test_legend_label
+                assert args['line_color'] == test_line_color
+                assert args['line_width'] == test_line_width
+
+        test_plotter._plot = TestPlotLine()
+
+        test_plotter.add_line(points=test_points,
+                              legend_label=test_legend_label,
+                              line_color=test_line_color,
+                              line_width=test_line_width,
+                              )
+
+
+class TestPlot:
+    @pytest.mark.parametrize(
+        'points',  # points/new line added
+        [None,  # no points/new line added
+         [(1, 2), (3, 4), (5, 6)]
+         ])
+    def test_points_added(self, points):
+        test_plotter = BokehPlotter()
+
+        # patch add_line method
+        add_line_called = False
+
+        def add_line_telemetry(*args, **kwargs):
+            nonlocal add_line_called  # Needed to assign var in scope
+            add_line_called = True
+
+        test_plotter.add_line = add_line_telemetry
+
+        # patch show_in_browser method
+        test_plotter.show_in_browser = lambda: None
+
+        test_plotter.plot(points)
+        assert add_line_called == bool(points)
